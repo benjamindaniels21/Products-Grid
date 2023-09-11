9b28ac9163b2f192d259f251dd953fc59643db94
Ben Daniels <benjamindaniels21@gmail.com>
2023-09-10 20:22:22 -0400

    restructure card classes

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,33 +1,36 @@
 /* eslint-disable react/jsx-key */
 import "./Products.css";
+import hat from "../assets/baseball-hat.jpg";
+
 const productsArray = [
   {
     id: 1,
-    name: "Baseball Cap",
+    title: "Baseball Cap",
+    image: hat,
     price: 15.99,
     description: "A baseball cap",
   },
   {
     id: 2,
-    name: "Jacket",
+    title: "Jacket",
     price: 139.99,
     description: "A jacket",
   },
   {
     id: 3,
-    name: "Jeans",
+    title: "Jeans",
     price: 55.99,
     description: "A pair of jeans",
   },
   {
     id: 4,
-    name: "Scarf",
+    title: "Scarf",
     price: 10.0,
     description: "A scarf",
   },
   {
     id: 5,
-    name: "T-shirt",
+    title: "T-shirt",
     price: 14.99,
     description: "A t-shirt",
   },
@@ -35,15 +38,23 @@ const productsArray = [
 
 const items = productsArray.map((item) => {
   return (
-    <div className="item">
-      <h2>{item.price}</h2>
-      <h2>{item.description}</h2>
+    <div className="card">
+      <img src={item.image} alt="" />
+      <div className="card__content">
+        <h2 className="card__title">{item.title}</h2>
+        <p className="card__price">{item.price}</p>
+        <p className="card__description">{item.description}</p>
+      </div>
     </div>
   );
 });
 
 const Products = () => {
-  return <div className="container">{items}</div>;
+  return (
+    <div className="container">
+      <div className="product-grid">{items}</div>
+    </div>
+  );
 };
 
 export default Products;
