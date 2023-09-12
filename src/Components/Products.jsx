9f28a6dd11db664c03af1646c48eb35476ffa118
Ben Daniels <benjamindaniels21@gmail.com>
2023-09-11 20:25:57 -0400

    product styling update

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/jsx-key */
 import "./Products.css";
 import hat from "../assets/baseball-hat.jpg";
+import jacket from "../assets/jacket.jpg";
+import jeans from "../assets/jeans.jpg";
+import scarf from "../assets/scarf.jpg";
+import tShirt from "../assets/t-shirt.jpg";
 
 const productsArray = [
   {
@@ -13,24 +17,28 @@ const productsArray = [
   {
     id: 2,
     title: "Jacket",
+    image: jacket,
     price: 139.99,
     description: "A jacket",
   },
   {
     id: 3,
     title: "Jeans",
+    image: jeans,
     price: 55.99,
     description: "A pair of jeans",
   },
   {
     id: 4,
     title: "Scarf",
+    image: scarf,
     price: 10.0,
     description: "A scarf",
   },
   {
     id: 5,
     title: "T-shirt",
+    image: tShirt,
     price: 14.99,
     description: "A t-shirt",
   },
@@ -38,11 +46,11 @@ const productsArray = [
 
 const items = productsArray.map((item) => {
   return (
-    <div className="card">
+    <div className="card stacked">
       <img src={item.image} alt="" />
       <div className="card__content">
         <h2 className="card__title">{item.title}</h2>
-        <p className="card__price">{item.price}</p>
+        <p className="card__price">${item.price}</p>
         <p className="card__description">{item.description}</p>
       </div>
     </div>
