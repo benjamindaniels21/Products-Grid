a4bbb0df6b09bc4115c544a291654451c00c8307
Ben Daniels <benjamindaniels21@gmail.com>
2023-09-13 08:17:35 -0400

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./Components/Header";
 import Products from "./Components/Products";
 
 function App() {
   return (
     <>
+      <Header />
       <Products />
     </>
   );
